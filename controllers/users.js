d50a05221c4e7020d4638e5cf3d89047705d9f5c
Oksana Shishova <oksi-love@yandex.ru>
2023-06-23 02:41:54 +0300

    13-1

--- controllers/users.js
+++ controllers/users.js
@@ -21,7 +21,15 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(CREATED).send(user))
-    .catch(err => res.status(BAD_REQUEST).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if {
+        res.status(BAD_REQUEST).send({
+          message: 'Пользователь уже зарегистрирован.',
+        });
+      } else {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные при регистрации пользователя' });
+      }
+    });
 };
 
 
