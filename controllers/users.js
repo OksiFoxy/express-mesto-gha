48f5a1d6b2f53832dfe38791969c80e1666e5b41
Oksana Shishova <oksi-love@yandex.ru>
2023-06-25 23:57:51 +0300

    13-1

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,5 @@
 const User = require('../models/userSchema');
 const { OK, CREATED, BAD_REQUEST, NOT_FOUND, SERVER_ERROR,} = require('../utils/constants');
-const ConflictError = require('../errors/ConflictError');
 const InaccurateDataError = require('../errors/InaccurateDataError');
 
 // Получение списка пользователей
@@ -39,16 +38,15 @@ module.exports.createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.status(CREATED).send(user))
     .catch((err) => {
-      if (err.code === 11000) {
-        next(new ConflictError('Пользователь уже зарегистрирован'));
-      } else if (err.name === 'ValidationError') {
-        next(new InaccurateDataError('Переданы некорректные данные при регистрации пользователя'));
+      if (err.name === 'CastError' || err.name === 'ValidationError') {
+        res.status(BAD_REQUEST).send({
+          message: 'Переданы некорректные данные при создании пользователя.',
+        });
       } else {
-        next(err);
+        res.status(SERVER_ERROR).send({ message: 'Ошибка по умолчанию' });
       }
     });
 };
-
 
 // Обновление профиля пользователя
 module.exports.updateUserData = (req, res, next) => {
@@ -58,21 +56,20 @@ module.exports.updateUserData = (req, res, next) => {
     { name, about },
     { new: true, runValidators: true },
   ).orFail().then((user) => res.status(OK).send(user))
-    .catch((err) => {
-      if (err.name === 'CastError' || err.name === 'ValidationError') {
-        return res
-          .status(BAD_REQUEST).send({
-            message: 'Переданы некорректные данные при обновлении профиля',
-          });
-      }
-      if (err.name === 'DocumentNotFoundError') {
-        return res
-          .status(NOT_FOUND).send({
-            message: 'Пользователь не найден',
-          });
-      }
-      return res.status(SERVER_ERROR).send({ message: 'Ошибка по умолчанию' });
-    });
+  .catch((err) => {
+    if (err.name === 'DocumentNotFoundError') {
+      return res.status(NOT_FOUND)
+        .send({ message: 'Пользователь с этим id не найден' });
+    }
+    if (err.name === 'CastError') {
+      return res
+        .status(BAD_REQUEST)
+        .send({
+          message: 'Переданы некорректные данные.',
+        });
+    }
+    return res.status(SERVER_ERROR).send({ message: 'Ошибка по умолчанию' });
+  });
 };
 // Обновление аватара пользователя
 module.exports.updateUserAvatar  = (req, res, next) => {
@@ -82,16 +79,15 @@ module.exports.updateUserAvatar  = (req, res, next) => {
     .orFail(new NotFound('Некорректный ID пользователя для обновления аватара.'))
     .then((newAvatar) => res.send({ data: newAvatar }))
     .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(NOT_FOUND)
+          .send({ message: 'Пользователь с этим id не найден' });
+      }
       if (err.name === 'CastError' || err.name === 'ValidationError') {
         return res
-          .status(BAD_REQUEST).send({
-            message: 'Переданы некорректные данные при обновлении аватара',
-          });
-      }
-      if (err.name === 'DocumentNotFoundError') {
-        return res
-          .status(NOT_FOUND).send({
-            message: 'Пользователь не найден',
+          .status(BAD_REQUEST)
+          .send({
+            message: 'Переданы некорректные данные.',
           });
       }
       return res.status(SERVER_ERROR).send({ message: 'Ошибка по умолчанию' });
